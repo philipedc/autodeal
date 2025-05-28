1ee6ba0c859c74f31365ca654cfb5595a6ad554b
Iago Gabino <iagogabinogoncalves@gmail.com>
2025-05-28 00:40:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AutoDeal
+Teste de Software 2025/1 - UFMG
+
+## Membros:
+- Arthur Linhares Madureira
+- Iago Gabino Gonçalves
+- Philipe Dutra Cunha
---
+++ api/server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Exemplo de rota
+app.get('/', (req, res) => {
+  res.send('API AutoDeal está rodando');
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
